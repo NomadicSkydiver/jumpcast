e454f407d1e96007de78878f24b91c9bcb5365b4
NomadicSkydiver <nomadicskydiver@gmail.com>
2026-09-16 19:35:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ app.get("/api/weather", async (req,res) => {
     "cloud_cover","visibility","wind_speed_10m","wind_direction_10m",
     "wind_gusts_10m","wind_speed_80m","wind_direction_80m",
     "wind_speed_100m","wind_direction_100m",
-    "wind_speed_900hPa","wind_direction_900hPa",
-    "wind_speed_700hPa","wind_direction_700hPa"
   ].join(",");
 
   const weatherUrl =
