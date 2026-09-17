f74b316b2ee0abf9e23a1dfcaa86120c433282c1
NomadicSkydiver <nomadicskydiver@gmail.com>
2026-09-17 13:41:19 -0400

    Create verified DZ location database

---
+++ verified-locations.js
@@ -0,0 +1,9 @@
+const VERIFIED_LOCATIONS = {
+  "Midwest Freefall Sport Parachute Club, Inc.": {
+    address: "62912 Kunstman Road, Ray, MI 48096",
+    lat: 42.75956,
+    lon: -82.94168
+  }
+};
+
+module.exports = VERIFIED_LOCATIONS;
