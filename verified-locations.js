3975f0c24ec55f844c0c0c890ce5975c3bb8e47e
NomadicSkydiver <nomadicskydiver@gmail.com>
2026-09-17 13:57:20 -0400

    Add Skydive Tecumseh verified location

--- verified-locations.js
+++ verified-locations.js
@@ -3,7 +3,11 @@ const VERIFIED_LOCATIONS = {
     address: "62912 Kunstman Road, Ray, MI 48096",
     lat: 42.75956,
     lon: -82.94168
+  },
+
+  "Skydive Tecumseh": {
+    address: "8607 Cady Road, Jackson, MI 49201",
+    lat: 42.174165,
+    lon: -84.261341
   }
 };
-
-module.exports = VERIFIED_LOCATIONS;
